5ab6c0064c053ed98f5bc6e4b0839060f4d933df
Halil <halillkesgin@gmail.com>
2024-01-07 17:00:46 +0300

    info card done

--- components/stream-player/index.tsx
+++ components/stream-player/index.tsx
@@ -11,6 +11,7 @@ import { Video, VideoSkeleton } from "./video"
 import { Chat, ChatSkeleton } from "./chat"
 import { ChatToggle } from "./chat-toggle"
 import { Header, HeaderSkeleton } from "./header"
+import { InfoCard } from "./info-card"
 
 interface StreamPlayerProps {
     user: User & { stream: Stream | null }
@@ -58,6 +59,12 @@ export const StreamPlayer = ({ user, stream, isFollowing }: StreamPlayerProps) =
                         imageUrl={user.imageUrl}
                         isFollowing={isFollowing}
                         name={stream.name}
+                    />
+                    <InfoCard 
+                        hostIdentity={user.id}
+                        viewerIdentity={identity}
+                        name={stream.name}
+                        thumbnailUrl={stream.thumbnailUrl}
                     />
                 </div>
                 <div className={cn(
