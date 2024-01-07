25ea0f4b5fe26b33ce97ab3246d9d20f752e8727
Halil <halillkesgin@gmail.com>
2024-01-08 02:01:11 +0300

    user page done

--- actions/block.ts
+++ actions/block.ts
@@ -1,17 +1,35 @@
 "use server"
 
 import { revalidatePath } from "next/cache"
+import { RoomServiceClient } from "livekit-server-sdk"
 
 import { blockUser, unblockUser } from "@/lib/block-service"
+import { getSelf } from "@/lib/auth-service"
+
+const roomService = new RoomServiceClient(
+    process.env.LIVEKIT_API_URL!,
+    process.env.LIVEKIT_API_KEY!,
+    process.env.LIVEKIT_API_SECRET!
+)
 
 export const onBlock = async (id: string) => {
-    const blockedUser = await blockUser(id)
-    
-    revalidatePath("/")
-    
-    if (blockedUser) {
-        revalidatePath(`/${blockedUser.blocked.username}`)
+    const self = await getSelf()
+
+    let blockedUser
+
+    try {
+        blockedUser = await blockUser(id)
+    } catch {
+        // this means user is a guest
     }
+
+    try {
+        await roomService.removeParticipant(self.id, id)
+    } catch {
+        // this means user is not in the room
+    }
+    
+    revalidatePath(`/u/${self.username}/community`)
     
     return blockedUser
 }
